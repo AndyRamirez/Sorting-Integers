3bc6d4ee38a83a40bf2fe6fca256df7429acc5e1
andy <kramirez08@alumnos.uaq.mx>
2018-11-12 15:33:31 -0800

    Added bubble sort algoritmh *Added 2 FOR LOOPS needed for the bubble sort algorith *Added if sentence for comparation

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 
 int main(){
-	int i,k=0;
+	int i,k,l,y=0;
 	int N=10;
+	int aux =0;
 	int array[N];	
 	//TODO: Read 10 numbers from user
 	for (i=0; i<10; i++){
@@ -13,8 +14,15 @@ int main(){
 	}
 
 	//TODO: Sort them 
-	//
-	//
+	for (l=0; l<10; l++){
+	for (y=0; y<10; y++){
+	if (array[y]>array[y+1]){
+		aux = array[y];
+		array[y]= array[y+1];
+		array[y+1]= aux;
+	}
+	}
+	}
 	//TODO: Print them
 	printf("impresion\n");
 	for (k=0; k<10; k++){
