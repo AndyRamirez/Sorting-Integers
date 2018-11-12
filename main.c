1ed679a8be071fb00037ec0f217b2c3a539a5925
andy <kramirez08@alumnos.uaq.mx>
2018-11-12 15:13:14 -0800

    Created initial version *Create the main.c file as main program

---
+++ main.c
@@ -0,0 +1,16 @@
+/* 
+ * This program reads 10 int from user, then it print this numbers sorted
+*/
+#include <iostream>
+
+int main(){
+	
+	//TODO: Read 10 numbers from user
+	//
+	//
+	//TODO: Sort them 
+	//
+	//
+	//TODO: Print them
+
+}
