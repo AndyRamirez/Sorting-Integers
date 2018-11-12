a5e4f81f0d5ae522af4808851c9b605757caa13a
andy <kramirez08@alumnos.uaq.mx>
2018-11-12 15:21:51 -0800

    Add FOR LOOP for ask 10 numbers to the user *Add FOR LOOP for ask 10 numbers *Add declaration of some variables to use in this program

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 /* 
  * This program reads 10 int from user, then it print this numbers sorted
 */
-#include <iostream>
+#include <stdio.h>
 
 int main(){
-	
+	int i=0;
+	int N=10;
+	int array[N];	
 	//TODO: Read 10 numbers from user
-	//
-	//
+	for (i=0; i<10; i++){
+		scanf ("%d", &array[i]);
+	}
+
 	//TODO: Sort them 
 	//
 	//
